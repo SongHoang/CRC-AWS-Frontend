584fed17903d50059dee662d67d923c72a2eaedb
isonghoang <isonghoang@gmail.com>
2025-02-27 10:50:22 -0500

    Okay CI/CD has to work now. Updated website code

--- react-template/out/_next/static/89Dcus_HwGum6DjLPYL5K/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/601-4abf5c524f4f194f.js","static/chunks/pages/index-32ca77e208519620.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
