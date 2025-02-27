e3c1cd63d834dc4ac261e37fb46ccf5d89edc1f1
isonghoang <isonghoang@gmail.com>
2025-02-27 11:17:12 -0500

    Fixed API url on website

--- react-template/out/_next/static/-ANk3oqYC6grzkWRu28hc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/122-25ffe40c79f84ff8.js","static/chunks/pages/index-c9bb7359e88f55fe.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
