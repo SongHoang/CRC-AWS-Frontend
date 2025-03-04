57036d3122458fef366b348a403af5f67fca149a
isonghoang <isonghoang@gmail.com>
2025-03-04 13:53:41 -0500

    Didn't add the rest of website files

---
+++ react-template/out/_next/static/uVbarDt_yhOopOOSD_HEr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/122-25ffe40c79f84ff8.js","static/chunks/pages/index-766a6ff1cb0e3178.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
